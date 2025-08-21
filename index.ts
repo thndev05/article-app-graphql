c582440081313450fb48dc3438471f4d843294da
Tran Hoang Nhat <kundeeptryvl@gmail.com>
2025-08-21 21:50:10 +0700

    feat: integrate express middleware for Apollo Server

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ import express, { Express, Request, Response } from 'express';
 import * as database from './config/database'
 import dotenv from 'dotenv';
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from '@as-integrations/express5';
+// import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from './typeDefs';
 import { resolvers } from './resolvers';
 
@@ -14,17 +15,21 @@ const startServer = async () => {
   const app: Express = express();
   const port: any = process.env.PORT || 3000;
 
+  app.use(express.json());
+
   // GraphQL
   const apolloServer = new ApolloServer({ typeDefs, resolvers });
+  await apolloServer.start();
 
-  // Khởi động GraphQL server riêng biệt  
-  const { url } = await startStandaloneServer(apolloServer, {
-    listen: { 
-      port: port 
-    } 
-  });
+  // Cách 1: Khởi động GraphQL server riêng biệt  
+  // const { url } = await startStandaloneServer(apolloServer, {
+  //   listen: { 
+  //     port: port 
+  //   }
+  // });
 
-  console.log(`🎯 GraphQL server ready at ${url}`);
+  // Cách 2: Khởi động GraphQL server với expressMiddleware from '@as-integrations/express5'
+  app.use('/graphql', expressMiddleware(apolloServer));
 
   app.listen(port, () => {
     console.log(`🚀 Server is running on http://localhost:${port}`);
